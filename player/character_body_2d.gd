f4aa7ccbbe797ce2ec5b569b565e28331af52aaf
ramoma <monecraftisgood@gmail.com>
2026-02-16 12:28:50 +0800

    changed movement to mouseclick, added sprite

--- player/character_body_2d.gd
+++ player/character_body_2d.gd
@@ -1,13 +1,20 @@
 extends CharacterBody2D
 
 var speed = 200
+var click_position = Vector2()
+var target_position = Vector2()
 
-func _physics_process(delta):
-	var input_direction = Vector2(
-		Input.get_action_strength("right") - Input.get_action_strength("left"),
-		Input.get_action_strength("down") - Input.get_action_strength("up")
-	)
+@onready var animation_tree = $AnimationTree
 
-	position += input_direction * delta * speed
+func _ready():
+	click_position = global_position
 	
-	move_and_slide()
+func _physics_process(_delta):
+	
+	if Input.is_action_just_pressed("left click"):
+		click_position = get_global_mouse_position()
+	
+	if position.distance_to(click_position) > 3:
+		target_position = (click_position - global_position).normalized()
+		velocity = target_position * speed
+		move_and_slide()
