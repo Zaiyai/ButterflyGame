1296c30b205dd42fdcf838fbf54925984ea72170
lyphexea <lyphexea@gmail.com>
2026-02-17 17:21:32 +0800

    Added pointlight + occlusion, Adjusted player movement(wip)

--- player/character_body_2d.gd
+++ player/character_body_2d.gd
@@ -3,6 +3,7 @@ extends CharacterBody2D
 var speed = 200
 var click_position = Vector2()
 var target_position = Vector2()
+var rotation_speed = 10
 
 @onready var animation_tree = $AnimationTree
 @onready var state_machine = animation_tree.get("parameters/playback")
@@ -11,12 +12,15 @@ func _ready():
 	click_position = global_position
 		
 	
-func _physics_process(_delta):
+func _physics_process(delta):
 	
 	if Input.is_action_just_pressed("left click"):
 		click_position = get_global_mouse_position()
-		look_at(get_global_mouse_position() )
-	
+		var target_dir = (click_position - global_position).angle()
+		if rotation != target_dir:
+			rotation = lerp_angle(global_rotation, target_dir, rotation_speed * delta)
+		else:
+			look_at(click_position)
 	if position.distance_to(click_position) > 10:
 		target_position = (click_position - global_position).normalized()
 		velocity = target_position * speed
