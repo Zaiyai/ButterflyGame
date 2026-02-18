036a7417c472a2d032080cdbd579968fd766e82c
lyphexea <lyphexea@gmail.com>
2026-02-18 11:03:32 +0800

    Adjusted The Path map size, Added Camera zoom out areas

--- player/character_body_2d.gd
+++ player/character_body_2d.gd
@@ -22,3 +22,10 @@ func _physics_process(_delta):
 		velocity = target_position * speed
 		
 		move_and_slide()
+
+func _on_camera_zoom_area_body_entered(body: Node2D) -> void:
+	$Camera2D.zoom = Vector2(1, 1);
+
+
+func _on_camera_zoom_area_body_exited(body: Node2D) -> void:
+	$Camera2D.zoom = Vector2(1.8,1.8);
