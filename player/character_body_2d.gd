5bcdc2bfc685f5e865b1f963f4e43001b30bea65
lyphexea <lyphexea@gmail.com>
2026-02-18 15:31:42 +0800

    Attempted to smoothen player movement

--- player/character_body_2d.gd
+++ player/character_body_2d.gd
@@ -1,40 +1,61 @@
 extends CharacterBody2D
 
+# Player Movement Variables
 var speed = 200
-var click_position = Vector2()
-var target_position = Vector2()
+var clickPosition = Vector2()
+var clickDistance = Vector2()
+var clickTargetRot: float = 0.0
+var rotationSpeed: float  = 8.0
 
+# Camera Zoom Control Variables
 var zoomOutSpd = 0.5;
 var zoomDefSpd = 0.8;
 var zoomDefault = Vector2(1.8,1.8)
 var zoomOut = Vector2(1,1);
 var playerInArea = false
 
-
+# Player Movement Animation Variables
 @onready var animation_tree = $AnimationTree
 @onready var state_machine = animation_tree.get("parameters/playback")
 
+# Mouse Position Check
 func _ready():
-	click_position = global_position
-		
-	
+	clickPosition = global_position
+
 func _physics_process(delta):
 
+# Mouse Position Check and Turn
 	if Input.is_action_just_pressed("left click"):
-		click_position = get_global_mouse_position()
-		look_at(get_global_mouse_position() )
+		clickPosition = get_global_mouse_position()
+		clickTargetRot = global_position.angle_to_point(clickPosition)
+		
+	rotation = lerp_angle(rotation, clickTargetRot, rotationSpeed * delta)
 	
-	if position.distance_to(click_position) > 10:
-		target_position = (click_position - global_position).normalized()
-		velocity = target_position * speed
+	if abs(angle_difference(rotation, clickTargetRot)) < 0.2:
+		rotation = clickTargetRot
+
+# Player Movement
+	
+	if position.distance_to(clickPosition) > 10:
+		var forwardFace: Vector2 = Vector2.RIGHT.rotated(rotation)
+		velocity = forwardFace * speed
 		
 		move_and_slide()
+	else:
+		velocity = Vector2.ZERO * delta
 		
+		move_and_slide()
+	
+	# if position.distance_to(clickPosition) > 10:
+	# else:
+
+# Camera Zoom Controller
 	if playerInArea:
 		$Camera2D.zoom = $Camera2D.zoom.lerp(zoomOut, zoomOutSpd * delta)
 	else:
 		$Camera2D.zoom = $Camera2D.zoom.lerp(zoomDefault, zoomDefSpd * delta)
 
+# Camera Zoom Area Checker
 func _on_camera_zoom_area_body_entered(body: Node2D) -> void:
 	playerInArea = true
 
