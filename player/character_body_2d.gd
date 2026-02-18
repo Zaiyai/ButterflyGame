9de918a9768c55d33a5d33967b7c7b5a591349a0
lyphexea <lyphexea@gmail.com>
2026-02-18 12:59:10 +0800

    Fixed Camera Zooming Areas

--- player/character_body_2d.gd
+++ player/character_body_2d.gd
@@ -4,6 +4,13 @@ var speed = 200
 var click_position = Vector2()
 var target_position = Vector2()
 
+var zoomOutSpd = 0.5;
+var zoomDefSpd = 0.8;
+var zoomDefault = Vector2(1.8,1.8)
+var zoomOut = Vector2(1,1);
+var playerInArea = false
+
+
 @onready var animation_tree = $AnimationTree
 @onready var state_machine = animation_tree.get("parameters/playback")
 
@@ -11,8 +18,8 @@ func _ready():
 	click_position = global_position
 		
 	
-func _physics_process(_delta):
-	
+func _physics_process(delta):
+
 	if Input.is_action_just_pressed("left click"):
 		click_position = get_global_mouse_position()
 		look_at(get_global_mouse_position() )
@@ -22,10 +29,14 @@ func _physics_process(_delta):
 		velocity = target_position * speed
 		
 		move_and_slide()
+		
+	if playerInArea:
+		$Camera2D.zoom = $Camera2D.zoom.lerp(zoomOut, zoomOutSpd * delta)
+	else:
+		$Camera2D.zoom = $Camera2D.zoom.lerp(zoomDefault, zoomDefSpd * delta)
 
 func _on_camera_zoom_area_body_entered(body: Node2D) -> void:
-	$Camera2D.zoom = Vector2(1, 1);
-
+	playerInArea = true
 
 func _on_camera_zoom_area_body_exited(body: Node2D) -> void:
-	$Camera2D.zoom = Vector2(1.8,1.8);
+	playerInArea = false
