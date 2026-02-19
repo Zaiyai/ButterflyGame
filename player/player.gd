acb7cb26cdf207523b24962e44bab1ea91c1a4d2
Cyclobl <edrian.albero0@gmail.com>
2026-02-20 00:29:09 +0800

    cleaned up player code and player can now move towards really close click locations

--- player/player.gd
+++ player/player.gd
@@ -9,6 +9,9 @@ var clickDistance = Vector2()
 var clickTargetRot: float = 0.0
 var rotationSpeed: float  = 6.0
 
+# Variable deciding if the click was close to the player
+var closeClick := false
+
 # Camera Zoom Control Variables
 var zoomOutSpd = 0.5;
 var zoomDefSpd = 0.8;
@@ -25,20 +28,33 @@ func _ready():
 	clickPosition = global_position
 
 func _physics_process(delta):
+	var forwardFace: Vector2 = Vector2.RIGHT.rotated(rotation)
+	
 # Mouse Position Check and Turn
 	if Input.is_action_just_pressed("left click"):
 		clickPosition = get_global_mouse_position()
 		clickTargetRot = global_position.angle_to_point(clickPosition)
+		closeClick = false
+		
+		if position.distance_to(clickPosition) <= 40:
+			closeClick = true
 
 # Player Movement
+	print(closeClick)
+	if !closeClick:
+		if position.distance_to(clickPosition) > 25:
+				var desired_angle = global_position.angle_to_point(clickPosition)
+				rotation = lerp_angle(rotation, desired_angle, rotationSpeed * delta)
+				velocity = velocity.move_toward(forwardFace * speed, acceleration * delta)
+		else:
+			velocity = velocity.move_toward(Vector2.ZERO, friction * delta)
 	
-	if position.distance_to(clickPosition) > 25:
-		var desired_angle = global_position.angle_to_point(clickPosition)
-		rotation = lerp_angle(rotation, desired_angle, rotationSpeed * delta)
-		var forwardFace: Vector2 = Vector2.RIGHT.rotated(rotation)
-		velocity = velocity.move_toward(forwardFace * speed, acceleration * delta)
-	else:
-		velocity = velocity.move_toward(Vector2.ZERO, friction * delta)
+	if closeClick:
+		if position.distance_to(clickPosition) > 5:
+			velocity = velocity.move_toward(position.direction_to(clickPosition) * speed, (acceleration * delta)/3)
+		else:
+			velocity = velocity.move_toward(Vector2.ZERO, friction * delta)
+	
 	move_and_slide()
 
 # Camera Zoom Controller
@@ -48,8 +64,8 @@ func _physics_process(delta):
 		$Camera2D.zoom = $Camera2D.zoom.lerp(zoomDefault, zoomDefSpd * delta)
 
 # Camera Zoom Area Checker
-func _on_camera_zoom_area_body_entered(body: Node2D) -> void:
+func _on_camera_zoom_area_body_entered(_body: Node2D) -> void:
 	playerInArea = true
 
-func _on_camera_zoom_area_body_exited(body: Node2D) -> void:
+func _on_camera_zoom_area_body_exited(_body: Node2D) -> void:
 	playerInArea = false
